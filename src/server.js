4b39ca76235fa9eb3800f5689ca86a07f44a9aac
hogsmill <info@hogsmill.com>
2021-02-24 10:42:43 +0000

    fix server

--- src/server.js
+++ src/server.js
@@ -19,16 +19,33 @@ ON_DEATH(function(signal, err) {
   })
 })
 
-const express = require('express')
-const app = express()
-const http = require('http').createServer(app)
-const io = require('socket.io')(http, {
-  cors: {
-    origins: ['http://localhost:*', 'http://agilesimulations.co.uk'],
-    methods: ['GET', 'POST'],
-    credentials: true
+let httpServer
+let io
+if (!prod) {
+  const express = require('express')
+  const app = express()
+  httpServer = require('http').createServer(app)
+  io = require('socket.io')(httpServer, {
+    cors: {
+      origins: ['http://localhost:*'],
+      methods: ['GET', 'POST'],
+      credentials: true
+    }
+  })
+} else {
+  const options = {
+    key: fs.readFileSync('/etc/ssl/private/agilesimulations.co.uk.key'),
+    cert: fs.readFileSync('/etc/ssl/certs/07DDA10F5A5AB75BD9E9508BC490D32C.cer')
   }
-})
+  httpServer = require('https').createServer(options)
+  io = require('socket.io')(httpServer, {
+    cors: {
+      origins: ['https://agilesimulations.co.uk'],
+      methods: ['GET', 'POST'],
+      credentials: true
+    }
+  })
+}
 
 const dbStore = require('./store/dbStore.js')
 
@@ -49,59 +66,42 @@ function emit(event, data) {
   io.emit(event, data)
 }
 
-function doDb(fun, data) {
-  MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
-    if (err) throw err
-    const db = client.db('db')
+MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
+  if (err) throw err
+  const db = client.db('db')
 
-    switch(fun) {
-      case 'loadGame':
-        dbStore.loadGame(db, io, data, debugOn)
-        break
-      case 'restartGame':
-        dbStore.restartGame(db, io, data, debugOn)
-        break
-      case 'addPlayer':
-        dbStore.addPlayer(db, io, data, debugOn)
-        break
-      case 'updateItems':
-        dbStore.updateItems(db, io, data, debugOn)
-        break
+  io.on('connection', (socket) => {
+    connections = connections + 1
+    if (connections > maxConnections) {
+      console.log(`Too many connections. Socket ${socket.id} closed`)
+      socket.disconnect(0)
+    } else {
+      connectDebugOff || console.log(`A user connected with socket id ${socket.id}. (${connections} connections)`)
+      emit('updateConnections', {connections: connections, maxConnections: maxConnections})
     }
+
+    socket.on('disconnect', () => {
+      connections = connections - 1
+      connectDebugOff || console.log(`User with socket id ${socket.id} has disconnected. (${connections} connections)`)
+      emit('updateConnections', {connections: connections, maxConnections: maxConnections})
+    })
+
+    socket.on('loadGame', (data) => { dbStore.loadGame(db, io, data, debugOn) })
+
+    socket.on('restartGame', (data) => { dbStore.restartGame(db, io, data, debugOn) })
+
+    socket.on('addPlayer', (data) => { dbStore.addPlayer(db, io, data, debugOn) })
+
+    socket.on('startGame', (data) => { emit('startGame', data) })
+
+    socket.on('updateItems', (data) => { dbStore.updateItems(db, io, data, debugOn) })
+
+    socket.on('endGame', (data) => { emit('endGame', data) })
   })
-}
-io.on('connection', (socket) => {
-  connections = connections + 1
-  if (connections > maxConnections) {
-    console.log(`Too many connections. Socket ${socket.id} closed`)
-    socket.disconnect(0)
-  } else {
-    connectDebugOff || console.log(`A user connected with socket id ${socket.id}. (${connections} connections)`)
-    emit('updateConnections', {connections: connections, maxConnections: maxConnections})
-  }
-
-  socket.on('disconnect', () => {
-    connections = connections - 1
-    connectDebugOff || console.log(`User with socket id ${socket.id} has disconnected. (${connections} connections)`)
-    emit('updateConnections', {connections: connections, maxConnections: maxConnections})
-  })
-
-  socket.on('loadGame', (data) => { doDb('loadGame', data) })
-
-  socket.on('restartGame', (data) => { doDb('restartGame', data) })
-
-  socket.on('addPlayer', (data) => { doDb('addPlayer', data) })
-
-  socket.on('startGame', (data) => { emit('startGame', data) })
-
-  socket.on('updateItems', (data) => { doDb('updateItems', data) })
-
-  socket.on('endGame', (data) => { emit('endGame', data) })
-
 })
 
 const port = process.argv[2] || 3011
 
-http.listen(port, () => {
+httpServer.listen(port, () => {
   console.log('Listening on *:' + port)
 })
