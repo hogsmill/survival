14200191acc97adfb47b07a7652bdff1174a696e
hogsmill <info@hogsmill.com>
2021-03-02 20:06:32 +0000

    https

--- src/App.vue
+++ src/App.vue
@@ -84,11 +84,12 @@ export default {
     }
   },
   created() {
-    let host = '77.68.122.69'
+    let connStr
     if (location.hostname == 'localhost') {
-      host = 'localhost'
+      connStr = 'http://localhost:3011'
+    } else {
+      connStr = 'https://agilesimulations.co.uk:3011'
     }
-    const connStr = 'http://' + host + ':3011'
     console.log('Connecting to: ' + connStr)
     this.socket = io(connStr)
 
