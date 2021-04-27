b3ed85180a22e31e75bdedc5b152b8aa3eb250ad
hogsmill <info@hogsmill.com>
2021-04-27 15:06:39 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,92 @@
 #!/bin/bash
 
-if [ "$1" == "-f" ]; then
-  FORCE=true
-fi
-
-PORT=3011
-PWD=`pwd`
-APP=`basename $PWD`
-git stash
-GIT=`git pull`
-echo $GIT
-if [ "$FORCE" != "true" -a "$GIT" == "Already up to date." ]; then
-  exit 0
-fi
-
-npm install
-npm run build
-rm /var/www/html/$APP/css/*
-rm /var/www/html/$APP/js/*
-cp -R dist/* /var/www/html/$APP
-if [ -f "src/server.js" ]; then
-  SERVER=`ps -ef | grep server.js | grep $PORT | awk {'print $2'}`
-  if [ "$SERVER" != "" ]; then
-    kill -9 $SERVER
+FORCE=false
+OUTDATED=true
+while [ $1 ]
+do
+  if [ "$1" == "-f" ]; then
+    FORCE=true
   fi
-fi
+  if [ "$1" == "-o" ]; then
+    OUTDATED=false
+  fi
+  shift
+done
 
-/usr/bin/php /usr/apps/monitor/src/lib/outdated.php &
+REPO="https://github.com/hogsmill/survival.git"
+APPS=(
+  'survival,survival,3011'
+  'survival-new,survivalNew,3026,Battleships New,12345'
+)
+
+for ((i = 0; i < ${#APPS[@]}; i++))
+do
+  REC="${APPS[$i]}"
+
+  APP=`echo $REC | cut -d, -f1`
+  COLLECTION=`echo $REC | cut -d, -f2`
+  PORT=`echo $REC | cut -d, -f3`
+  APPNAME=`echo $REC | cut -d, -f4`
+  PASSWORD=`echo $REC | cut -d, -f5`
+
+  echo "------------------------------------------------"
+  if [ -z "$APPNAME" ]; then
+    echo "Installing $APP ($COLLECTION, $PORT)"
+  else
+    echo "Installing $APP ($COLLECTION, $PORT, $APPNAME, $PASSWORD)"
+  fi
+  echo "------------------------------------------------"
+
+  DIR="/usr/apps/$APP"
+  if [ ! -d $DIR ]; then
+    git clone $REPO $DIR
+  fi
+  ENVFILE="$DIR/.env"
+  echo "VUE_APP_PORT=$PORT" > $ENVFILE
+  echo "VUE_APP_COLLECTION=$COLLECTION" >> $ENVFILE
+  if [ ! -z "$APPNAME" ]; then
+    echo "VUE_APP_NAME=$APPNAME" >> $ENVFILE
+  fi
+  if [ ! -z $PASSWORD ]; then
+    echo "VUE_APP_PASSWORD=$PASSWORD" >> $ENVFILE
+  fi
+
+  cd $DIR
+
+  PWD=`pwd`
+  APP=`basename $PWD`
+  git stash
+  GIT=`git pull`
+  echo $GIT
+  if [ "$FORCE" != "true" -a "$GIT" == "Already up to date." ]; then
+    exit 0
+  fi
+
+  npm install --legacy-peer-deps
+  npm run build
+  if [ ! -d /var/www/html/$APP/ ]; then
+    mkdir /var/www/html/$APP
+  fi
+  if [ -d /var/www/html/$APP/css ]; then
+    rm /var/www/html/$APP/css/*
+  else
+    mkdir /var/www/html/$APP/css
+  fi
+  if [ -d /var/www/html/$APP/js ]; then
+    rm /var/www/html/$APP/js/*
+  else
+    mkdir /var/www/html/$APP/js
+  fi
+  cp -R dist/* /var/www/html/$APP
+  if [ -f "src/server.js" ]; then
+    SERVER=`ps -ef | grep server.js | grep "/$APP/" | awk {'print $2'}`
+    if [ "$SERVER" != "" ]; then
+      kill -9 $SERVER
+    fi
+  fi
+
+done
+
+if [ "$OUTDATED" == "true" ]; then
+  php /usr/apps/monitor/src/lib/outdated.php &
+fi
